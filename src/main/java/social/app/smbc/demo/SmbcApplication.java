3f6484fcb5fbb2a7dd15334dd194a29f84b25a5d
samir <s-malik@live.co.uk>
2023-03-07 18:27:20 +0000

    Initial spring project set-up

---
+++ src/main/java/social/app/smbc/demo/SmbcApplication.java
@@ -0,0 +1,13 @@
+package social.app.smbc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/social/app/smbc/demo/SmbcApplicationTests.java
@@ -0,0 +1,13 @@
+package social.app.smbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
