fda4573d438924f82d69be6b36b776e2fff7bd65
samir <s-malik@live.co.uk>
2023-03-07 19:08:34 +0000

    Implemented User & User Repository using commandline runner.

--- src/main/java/social/app/smbc/demo/SmbcApplication.java
+++ src/main/java/social/app/smbc/demo/SmbcApplication.java
@@ -1,13 +1,56 @@
 package social.app.smbc.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmbcApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SmbcApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmbcApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(UserRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new User("Jack", "Bauer"));
+			repository.save(new User("Chloe", "O'Brian"));
+			repository.save(new User("Kim", "Bauer"));
+			repository.save(new User("David", "Palmer"));
+			repository.save(new User("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (User user : repository.findAll()) {
+				log.info(user.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			User user = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(user.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			// for (Customer bauer : repository.findByLastName("Bauer")) {
+			//  log.info(bauer.toString());
+			// }
+			log.info("");
+		};
+	}
 }
---
+++ src/main/java/social/app/smbc/demo/User.java
@@ -0,0 +1,56 @@
+package social.app.smbc.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected User() {}
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "social.app.smbc.demo.User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
