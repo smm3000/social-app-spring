e18d0507f8fd2ada02b546082c60827658653805
Ben Coyne <bencoyne123@yahoo.co.uk>
2023-03-28 18:48:14 +0100

    wip controller, service, repository integration

--- src/main/java/social/app/smbc/demo/UserController.java
+++ src/main/java/social/app/smbc/demo/UserController.java
@@ -1,20 +1,29 @@
 package social.app.smbc.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class UserController {
 
+    @Autowired
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
     @GetMapping("/users/{id}")
-    String user(@PathVariable Long id) {
-        return userService.getUser();
+    Optional<User> user(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 }
--- src/main/java/social/app/smbc/demo/UserService.java
+++ src/main/java/social/app/smbc/demo/UserService.java
@@ -1,14 +1,30 @@
 package social.app.smbc.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private static UserRepository userRepository;
 
     public UserService() {
     }
 
     public String getUser() {
         return "User-test";
+    }
+
+    public Optional<User> getUserById(Long id) {
+
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
     }
 }
