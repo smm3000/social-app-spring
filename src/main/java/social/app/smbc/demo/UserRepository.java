079ee0fa4cd53d07c578c295be93892fa81848f1
Ben Coyne <bencoyne123@yahoo.co.uk>
2023-03-29 14:59:51 +0100

    get user and users endpoint functioning

--- src/main/java/social/app/smbc/demo/UserRepository.java
+++ src/main/java/social/app/smbc/demo/UserRepository.java
@@ -1,11 +1,9 @@
 package social.app.smbc.demo;
 
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findByLastName(String lastName);
-
     User findById(long id);
 }
--- src/main/java/social/app/smbc/demo/UserService.java
+++ src/main/java/social/app/smbc/demo/UserService.java
@@ -1,14 +1,29 @@
 package social.app.smbc.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    public UserService() {
-    }
+    private final UserRepository userRepository;
 
     public String getUser() {
         return "User-test";
+    }
+
+    public Optional<User> getUserById(Long id) {
+
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
     }
 }
