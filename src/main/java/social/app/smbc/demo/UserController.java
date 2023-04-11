7c41ff8c4a372c048cef20d3ca640e7068aa1265
samir <s-malik@live.co.uk>
2023-04-11 18:29:53 +0100

    Implemented Post & Delete User Mapping

--- src/main/java/social/app/smbc/demo/UserController.java
+++ src/main/java/social/app/smbc/demo/UserController.java
@@ -2,6 +2,7 @@ package social.app.smbc.demo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +24,16 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     Optional<User> user(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    @PostMapping
+    ResponseEntity <User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    private void deleteUser(@PathVariable Long id) {
+        userService.delete(id);
     }
 }
